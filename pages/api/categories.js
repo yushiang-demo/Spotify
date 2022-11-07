045c5c0016e74eebd362d3aca006af59c4b4b3b3
yushiang.tseng <yuhsinag.tseng@kdanmobile.com>
2022-11-07 08:58:09 +0800

    refactor : remove testing api and pages

--- pages/api/categories.js
+++
@@ -1,7 +0,0 @@
-import getCategories from "../../api/categories";
-
-export default async function handler(req, res) {
-  const { token } = req.query;
-  const categories = await getCategories(token);
-  res.status(200).json({ categories });
-}
--- pages/api/token.js
+++
@@ -1,9 +0,0 @@
-import getSpotifyToken from "../../api/token";
-
-export default async function handler(req, res) {
-  const token = await getSpotifyToken(
-    process.env.SPOTIFY_CLIENT_ID,
-    process.env.SPOTIFY_CLIENT_SECRET,
-  );
-  res.status(200).json({ token });
-}
