cc55a8c708fcd30916e97f27b74956317c856e2f
yushiang <tsengyushiang@gmail.com>
2022-11-12 02:38:30 +0800

    fix : server-side style

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,6 @@ import Head from "next/head";
 import { Provider } from "react-redux";
 
 import store from "../config/configureStore";
-import "../styles/body.css";
 
 function MyApp({ Component, pageProps }) {
   return (
